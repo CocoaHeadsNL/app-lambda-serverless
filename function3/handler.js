660bbebea6243f92239c6f675f4a977e6788eca8
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2017-10-18 09:48:04 +0200

    Refactor to new serverless config style.

--- function3/handler.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports.handler = function(event, context, cb) {
-  cb("test")
-  // return cb(null, {
-  //   message: 'Go Serverless! Your Lambda function executed successfully!'
-  // });
-};
